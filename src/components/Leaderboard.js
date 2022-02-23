8daae26a293aec83c40ecd030112b9d4f6923487
Patrik <patrikcumurdzic@gmail.com>
2022-02-23 04:47:18 +0100

    tabl

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -7,6 +7,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { dividerClasses } from "@mui/material";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -38,12 +39,23 @@ const rows = [
   createData("Eclair", 262, 16.0, 24, 6.0),
   createData("Cupcake", 305, 3.7, 67, 4.3),
   createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
 ];
 
 function Leaderboard() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+    <div className="leaderboard">
+      <TableContainer component={Paper} sx={{maxWidth: 1500, minHeight: 600}} >
+      <Table aria-label="customized table" >
         <TableHead>
           <TableRow>
             <StyledTableCell>Users</StyledTableCell>
@@ -76,6 +88,8 @@ function Leaderboard() {
         </TableBody>
       </Table>
     </TableContainer>
+    </div>
+    
   );
 }
 
